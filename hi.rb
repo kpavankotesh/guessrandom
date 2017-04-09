2f242d52da2544bc1b9af4787cc7158e252f68a6
Pavan Kotesh <pavan@keepworks.com>
2017-04-09 16:09:42 +0530

    Number guessing game complete

--- hi.rb
+++ hi.rb
@@ -2,6 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/hi' do
-  number = rand(100)
-  erb :index, :locals => {:number => number}
+  number = rand(10)
+  guess = params["guess"].to_i
+  message = guess_message(guess, number)
+
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+def guess_message(guess = 2, number = 1)
+  if guess > number 
+    is_high = "Too High!"
+  else
+    if guess < number
+      is_high = "Too Low!" 
+    else 
+      is_high = "Equal"
+    end
+  end
+
+  return is_high
 end
